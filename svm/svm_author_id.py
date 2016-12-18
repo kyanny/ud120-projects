14ccc9dfb7b347e4fd7672f670bbfc812acb2840
Kensuke Nagae <kyanny@gmail.com>
2016-12-19 01:48:53 +0900

    Commit SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -24,7 +24,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+# clf = SVC(kernel='linear')
+clf = SVC(C=10000., kernel='rbf')
+
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+predictions = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+print(clf.score(features_test, labels_test))
+
+# print(clf.predict(features_test[10]))
+# print(clf.predict(features_test[26]))
+# print(clf.predict(features_test[50]))
+
+print(len(filter((lambda x: x == 1), predictions)))
 
 #########################################################
-
-
